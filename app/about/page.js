0c0f7997febc8a2d771fcfb65a22215eb855f4ec
raindropplus <raindropplus11@gmail.com>
2024-05-18 03:55:28 +0600

    metadata add

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,17 @@
+import Button from "../components/button";
+import Image from "next/image";
+import nature from "@/public/images/oip.jpeg"
+
+export const metadata = {
+    title: "About",
+    description: "This is about page",
+  };
+
 export default function About(){
     // throw new Error('new error...')
     return (<main className="mt-10">
         <div>this is about page.</div>
+        <Image placeholder="blur" src={nature} alt="no image" />
+        <Button/>
     </main>); 
 }
